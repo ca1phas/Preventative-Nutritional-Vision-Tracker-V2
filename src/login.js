eaa6aa5ea94a1589a594f690cbb4ed07e7dd5225
ca1phas <65637919+ca1phas@users.noreply.github.com>
2026-03-14 17:38:44 +0800

    login fix

--- src/login.js
+++ src/login.js
@@ -3,17 +3,14 @@ import { initAuthGuard } from './auth-guard.js';
 initAuthGuard();
 
 document.getElementById('loginForm').addEventListener('submit', async (e) => {
-  document.getElementById('loginForm').addEventListener('submit', async (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    try {
-      const email = document.getElementById('email').value.trim();
-      const password = document.getElementById('password').value;
-      await authenticateUser(email, password)
-      window.location.href = 'userProfile.html';
-    } catch (err) {
-      alert('Invalid user ID or password. Please try again.');
-    }
-  });
-
-//Login Done 14/3/26
+  try {
+    const email = document.getElementById('email').value.trim();
+    const password = document.getElementById('password').value;
+    await authenticateUser(email, password)
+    window.location.href = 'userProfile.html';
+  } catch (err) {
+    alert('Invalid user ID or password. Please try again.');
+  }
+})
